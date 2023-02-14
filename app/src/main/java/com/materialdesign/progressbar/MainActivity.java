c55dc60031651e5925294d873dce2b4872722f17
Luis Fernando Machado <>
2023-02-14 20:37:52 -0300

    Using a circular progressbar

--- app/src/main/java/com/materialdesign/progressbar/MainActivity.java
+++ app/src/main/java/com/materialdesign/progressbar/MainActivity.java
@@ -2,6 +2,7 @@ package com.materialdesign.progressbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
@@ -17,5 +18,34 @@ public class MainActivity extends AppCompatActivity {
 
 		progress_circular = findViewById(R.id.progress_circular);
 		progress_circular.setVisibility(View.GONE);
+
+		new AsyncCircular().execute();
+	}
+
+	public class AsyncCircular extends AsyncTask<Void, Integer, Void> {
+
+		@Override
+		protected void onPreExecute() {
+			progress_circular.setVisibility(View.VISIBLE);
+		}
+
+		@Override
+		protected Void doInBackground(Void... voids) {
+
+			for (int i = 0; i < 40; i++) {
+				try {
+					Thread.sleep(300);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void unused) {
+			progress_circular.setVisibility(View.GONE);
+		}
 	}
 }
