0063719ff8f405163dbb482900e8c30be122558f
Luis Fernando Machado <>
2023-02-14 20:28:49 -0300

    Remove the visibility of the circular progressBar

--- app/src/main/java/com/materialdesign/progressbar/MainActivity.java
+++ app/src/main/java/com/materialdesign/progressbar/MainActivity.java
@@ -3,12 +3,19 @@ package com.materialdesign.progressbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+	private ProgressBar progress_circular;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		progress_circular = findViewById(R.id.progress_circular);
+		progress_circular.setVisibility(View.GONE);
 	}
 }
